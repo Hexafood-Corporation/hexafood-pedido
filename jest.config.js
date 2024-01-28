5f3435c9d69ba75a007613f59a221a91d51e75be
Bruno Padilha <brpadilha.dev@gmail.com>
2024-01-28 12:46:41 -0300

    Adicionar moduleNameMapper a jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   rootDir: './',
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
-
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
   transform: {
     '^.+.tsx?$': 'ts-jest',
   },

c18694cc1df4a7008280707a11077873d51a46fa
Bruno Padilha <brpadilha.dev@gmail.com>
2024-01-28 16:53:06 -0300

    Refactor pedidoUseCase files

--- src/pedido/core/application/usecases/pedidoUseCase/pedidos.pendentes.usecase.ts
+++ src/pedido/core/application/usecases/pedidoUseCase/pedidos.pendentes.usecase.ts
@@ -1,15 +1,15 @@
-import { IPedidosRepository } from "src/pedido/core/domain/repository/pedidos.repository";
+import { IPedidosRepository } from 'src/pedido/core/domain/repository/pedidos.repository';
 import { Inject } from '@nestjs/common';
-import { StatusPedido } from "src/pedido/core/domain/enum/status-pedido.enum";
-import { OutputPedidoDTO } from "./pedido.dto";
+import { StatusPedido } from 'src/pedido/core/domain/enum/status-pedido.enum';
+import { OutputPedidoDTO } from './pedido.dto';
 
 export class PedidosPendentesUseCase {
-    constructor(
-      @Inject(IPedidosRepository)
-      private pedidosRepository: IPedidosRepository,
-    ) {}
-  
-async execute(): Promise<OutputPedidoDTO[]> {
+  constructor(
+    @Inject(IPedidosRepository)
+    private pedidosRepository: IPedidosRepository,
+  ) {}
+
+  async execute(): Promise<OutputPedidoDTO[]> {
     return await this.pedidosRepository.findAll(StatusPedido.RECEBIDO);
   }
 }
---
+++ src/test/pedido/usecases/pedido/iniciar.preparacao.usecase.test.ts
@@ -0,0 +1,5 @@
+describe('IniciarPreparacaoUseCase', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true);
+  });
+});

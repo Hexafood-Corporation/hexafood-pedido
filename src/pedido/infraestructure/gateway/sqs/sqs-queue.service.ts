147a41771e0c52f12ecfeb98e1a7f4accc76acc8
Lucas Siqueira <lucassouza.ti@gmail.com>
2024-03-16 09:19:11 -0300

    feat: add session token for sqs client

--- src/pedido/infraestructure/gateway/sqs/sqs-queue.service.ts
+++ src/pedido/infraestructure/gateway/sqs/sqs-queue.service.ts
@@ -10,6 +10,7 @@ export class SqsQueueService  implements IQueueService{
             credentials: {
                 accessKeyId: process.env.AWS_ACCESS_KEY_ID,
                 secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                sessionToken: process.env.AWS_SESSION_TOKEN
             },
             endpoint: process.env.AWS_SQS_ENDPOINT,
         });
